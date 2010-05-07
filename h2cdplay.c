3fda232451347ae30ebea73eb1acc1fa51bb1da7
Vladislav Naumov <vnaum@vnaum.com>
2010-05-08 00:05:34 +0700

    indent, jingles in background

--- h2cdplay.c
+++ h2cdplay.c
@@ -11,20 +11,21 @@
 #define MAXTRACK 64
 HANDLE evt[MAXTRACK];
 
-int log_str(const char *format, ...)
+int
+log_str (const char *format, ...)
 {
   char timebuf[100];
   va_list ap;
   struct tm *tmp;
   time_t t;
 
-  t = time(NULL);
-  tmp = localtime(&t);
-  strftime(timebuf, sizeof(timebuf), "%H:%M:%S", tmp);
-  printf("[%s] ", timebuf);
+  t = time (NULL);
+  tmp = localtime (&t);
+  strftime (timebuf, sizeof (timebuf), "%H:%M:%S", tmp);
+  printf ("[%s] ", timebuf);
 
-  va_start(ap, format);
-  return vprintf(format, ap);
+  va_start (ap, format);
+  return vprintf (format, ap);
 }
 
 void
@@ -73,8 +74,8 @@ main (int argc, char **argv)
   int i;
   char *trkpath = "./tracks";
   char trkbuf[1024];
-  int *t_savepos;  // "save position" flag
-  QWORD *t_position; // saved position
+  int *t_savepos;               // "save position" flag
+  QWORD *t_position;            // saved position
   float volume = 1.0;
 
   printf ("HOMM2 CD music player\n");
@@ -92,8 +93,8 @@ main (int argc, char **argv)
   create_events ();
 
   // initialize t_savepos / t_position, set to zero
-  t_savepos  = calloc(MAXTRACK, sizeof(t_savepos[0]));
-  t_position = calloc(MAXTRACK, sizeof(t_position[0]));
+  t_savepos = calloc (MAXTRACK, sizeof (t_savepos[0]));
+  t_position = calloc (MAXTRACK, sizeof (t_position[0]));
 
   // Reset all pending events:
   for (i = 0; i < MAXTRACK; i++)
@@ -103,7 +104,8 @@ main (int argc, char **argv)
   printf ("Waiting for events...\n");
   while (1)
     {
-      DWORD dwWaitResult = WaitForMultipleObjects (MAXTRACK, evt, FALSE, INFINITE);
+      DWORD dwWaitResult =
+        WaitForMultipleObjects (MAXTRACK, evt, FALSE, INFINITE);
 
       log_str ("Event fired: %ld\n", dwWaitResult);
       trknum = dwWaitResult;
@@ -125,40 +127,66 @@ main (int argc, char **argv)
       // process playback events:
       if (trknum >= 2 && trknum <= 49)
         {
-          
-          if(t_savepos[now_playing])
-          {
-            t_position[now_playing] = BASS_ChannelGetPosition(chan, BASS_POS_BYTE);
-            log_str ("Saved position for track #%02d (%08ld)\n", now_playing, t_position[now_playing]);
-          }
+          // preload next chan
+          DWORD next = 0;
+          int next_len = -1;
 
-          StopChan (chan);
-          
-          now_playing = trknum;
-          
           sprintf (trkbuf, "%s/track%02d.mp3", trkpath, trknum);
-          if (t_savepos[now_playing])
-            log_str ("Playing track %s, volume = %.2f (saved position = %08ld)\n", trkbuf, volume, t_position[now_playing]);
-          else
-            log_str ("Playing track %s, volume = %.2f\n", trkbuf, volume);
-
-          chan =
+          next =
             BASS_StreamCreateFile (FALSE, trkbuf, 0, 0, BASS_STREAM_AUTOFREE);
-          if (!chan)
+          if (!next)
             {
               log_str ("Error while opening\n");
               continue;
             }
 
+          // check length
+          {
+            QWORD pos = BASS_ChannelGetLength (next, BASS_POS_BYTE);
+            if (pos != -1)
+              next_len = (DWORD) BASS_ChannelBytes2Seconds (next, pos);
+          }
+          // "jingle"? play it and forget it
+          if (next_len <= 10)
+            {
+              log_str
+                ("%s is a jingle (%d sec), playing in background\n",
+                 trkbuf, next_len);
+              BASS_ChannelPlay (next, FALSE);
+              continue;
+            }
+
+          // okay, it's not a jingle. do full-fledged processing (stop, restore, etc).
+          if (t_savepos[now_playing])
+            {
+              t_position[now_playing] =
+                BASS_ChannelGetPosition (chan, BASS_POS_BYTE);
+              log_str ("Saved position for track #%02d (%08ld)\n",
+                       now_playing, t_position[now_playing]);
+            }
+
+          StopChan (chan);
+          chan = next;
+          now_playing = trknum;
+
+          if (t_savepos[now_playing])
+            log_str
+              ("Playing track %s, volume = %.2f (saved position = %08ld)\n",
+               trkbuf, volume, t_position[now_playing]);
+          else
+            log_str ("Playing track %s, volume = %.2f\n", trkbuf, volume);
+
+
           // set volume:
           BASS_ChannelSetAttribute (chan, BASS_ATTRIB_VOL, volume);
           // seek to start:
           if (t_savepos[now_playing] && t_position[now_playing] > 0)
-          {
-            if ( ! BASS_ChannelSetPosition(chan, t_position[now_playing], BASS_POS_BYTE))
-              Error("Seek failed");
-          }
-          
+            {
+              if (!BASS_ChannelSetPosition
+                  (chan, t_position[now_playing], BASS_POS_BYTE))
+                Error ("Seek failed");
+            }
+
           BASS_ChannelPlay (chan, FALSE);
           continue;
         }
@@ -177,10 +205,11 @@ main (int argc, char **argv)
             BASS_ChannelSetAttribute (chan, BASS_ATTRIB_VOL, volume);
           continue;
         }
-      
+
       if (trknum == 61)
         {
-          log_str ("Setting 'save position' flag for track %d\n", now_playing);
+          log_str ("Setting 'save position' flag for track %d\n",
+                   now_playing);
           t_savepos[now_playing] = 1;
           continue;
         }
